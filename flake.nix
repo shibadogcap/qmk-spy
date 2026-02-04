eccd233a9d24c4092103c14635be8f50e2df5c7f
shibadogcap <yuuki1315s@gmail.com>
2026-02-04 16:08:07 +0900

    add pages

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "QMK-SPY development shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs_20
+              git
+              python3
+              gnumake
+              gcc
+              cmake
+              pkg-config
+            ];
+          };
+        });
+    };
+}
